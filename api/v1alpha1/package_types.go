a7c97971d425f8ba6dfbc2f1d4ddf246cfbe3184
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-01-15 16:41:58 +0100

    feat(package-operator): change package to be cluster scoped

--- api/v1alpha1/package_types.go
+++ api/v1alpha1/package_types.go
@@ -40,6 +40,7 @@ type PackageStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
 
 // Package is the Schema for the packages API
 type Package struct {
