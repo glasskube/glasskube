356c13ed4e83e0047712d53b70351027835e64d6
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-02-01 14:11:48 +0100

    ci: move manifest generation from workflow step to goreleaser hook
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

---
+++ hack/generate-manifests.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+CONFIG_DIR="config"
+OUT_DIR="build/manifest"
+rm -rf "$OUT_DIR"
+mkdir -p "$OUT_DIR"
+for VARIANT in "aio" "slim"; do
+  OUT_FILE="$OUT_DIR/manifest-$VARIANT.yaml ..."
+  echo "Generating $OUT_FILE"
+  kubectl kustomize "$CONFIG_DIR/$VARIANT" > "$OUT_FILE"
+done
