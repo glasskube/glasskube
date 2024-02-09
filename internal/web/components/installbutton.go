ce4e52ec0034b2fa719fd62c985935e93cad1580
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-02-09 15:51:24 +0100

    feat(ui): add open button on list page

--- internal/web/components/installbutton.go
+++ internal/web/components/installbutton.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 
+	"github.com/glasskube/glasskube/api/v1alpha1"
 	"github.com/glasskube/glasskube/pkg/client"
 	"github.com/glasskube/glasskube/pkg/list"
 )
@@ -18,13 +19,14 @@ func GetSwap(buttonId string) string {
 	return fmt.Sprintf("outerHTML:#%s", buttonId)
 }
 
-func RenderInstallButton(w io.Writer, tmpl *template.Template, pkgName string, status *client.PackageStatus) {
+func RenderInstallButton(w io.Writer, tmpl *template.Template, pkgName string, status *client.PackageStatus, manifest *v1alpha1.PackageManifest) {
 	buttonId := GetButtonId(pkgName)
 	err := tmpl.ExecuteTemplate(w, "installbutton", &map[string]any{
 		"ButtonId":    buttonId,
 		"Swap":        GetSwap(buttonId),
 		"PackageName": pkgName,
 		"Status":      status,
+		"Manifest":    manifest,
 	})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "An error occurred rendering the install button for %v: \n%v\n"+
@@ -39,5 +41,6 @@ func ToInstallButtonInput(pkgTeaser list.PackageTeaserWithStatus) map[string]any
 		"Swap":        GetSwap(buttonId),
 		"PackageName": pkgTeaser.PackageName,
 		"Status":      pkgTeaser.Status,
+		"Manifest":    pkgTeaser.InstalledManifest,
 	}
 }
