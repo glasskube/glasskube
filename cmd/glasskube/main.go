e665a8c82035545dfb3687eb1be8e611999c9d80
Baalekshan <69910615+Baalekshan@users.noreply.github.com>
2024-02-10 10:09:21 +0000

    feat: added a feature to detect outdated client for cli (#210)

--- cmd/glasskube/main.go
+++ cmd/glasskube/main.go
@@ -5,9 +5,11 @@ import (
 	"os"
 
 	"github.com/glasskube/glasskube/cmd/glasskube/cmd"
+	"github.com/glasskube/glasskube/internal/cliutils"
 )
 
 func main() {
+	cliutils.UpdateFetch()
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
