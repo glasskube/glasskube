f044a32423cc9f0c6233dad6fa76cab8d6e4d294
fykaa <faeka6@gmail.com>
2024-02-18 18:17:42 +0530

    feat: added Uninstall Blocking, Progress Spinner and enhanced CLI UI for Uninstall Command hash168

--- pkg/uninstall/uninstall.go
+++ pkg/uninstall/uninstall.go
@@ -2,18 +2,75 @@ package uninstall
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/glasskube/glasskube/api/v1alpha1"
 	"github.com/glasskube/glasskube/pkg/client"
+	"github.com/glasskube/glasskube/pkg/statuswriter"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/watch"
 )
+
+type uninstaller struct {
+	client *client.PackageV1Alpha1Client
+	status statuswriter.StatusWriter
+	pkg    *v1alpha1.Package
+}
 
 var deletePropagationForeground = metav1.DeletePropagationForeground
 
-func Uninstall(pkgClient *client.PackageV1Alpha1Client, ctx context.Context, pkg *v1alpha1.Package) error {
-	err := pkgClient.Packages().Delete(ctx, pkg, metav1.DeleteOptions{PropagationPolicy: &deletePropagationForeground})
+func NewUninstaller(pkgClient *client.PackageV1Alpha1Client, pkg *v1alpha1.Package) *uninstaller {
+	return &uninstaller{client: pkgClient, status: statuswriter.Noop(), pkg: pkg}
+}
+
+func (obj *uninstaller) WithStatusWriter(sw statuswriter.StatusWriter) *uninstaller {
+	obj.status = sw
+	return obj
+}
+
+// UninstallBlocking deletes the v1alpha1.Package custom resource from the
+// cluster and waits until the package is fully deleted.
+func (obj *uninstaller) UninstallBlocking(ctx context.Context, packageName string) error {
+	obj.status.Start()
+	defer obj.status.Stop()
+	pkgUID, err := obj.delete(ctx, packageName, obj.pkg)
 	if err != nil {
 		return err
 	}
-	return nil
+	return obj.awaitDeletion(ctx, pkgUID)
+}
+
+// Uninstall deletes the v1alpha1.Package custom resource from the cluster.
+func (obj *uninstaller) Uninstall(ctx context.Context, packageName string) error {
+	obj.status.Start()
+	defer obj.status.Stop()
+	_, err := obj.delete(ctx, packageName, obj.pkg)
+	return err
+}
+
+func (obj *uninstaller) delete(ctx context.Context, packageName string, pkg *v1alpha1.Package) (types.UID, error) {
+	obj.status.SetStatus(fmt.Sprintf("Uninstalling %v...", packageName))
+	err := obj.client.Packages().Delete(ctx, pkg, metav1.DeleteOptions{PropagationPolicy: &deletePropagationForeground})
+	if err != nil {
+		return "", err
+	}
+	return pkg.GetUID(), nil
+}
+
+func (obj *uninstaller) awaitDeletion(ctx context.Context, pkgUID types.UID) error {
+	watcher, err := obj.client.Packages().Watch(ctx)
+	if err != nil {
+		return err
+	}
+	defer watcher.Stop()
+	for event := range watcher.ResultChan() {
+		if obj, ok := event.Object.(*v1alpha1.Package); ok && obj.GetUID() == pkgUID {
+			if event.Type == watch.Deleted {
+				return nil // Package deletion confirmed
+			}
+		}
+	}
+	return errors.New("failed to confirm package deletion")
 }
