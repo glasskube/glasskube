54be740aae7d435b96adb37df9624e947024019d
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-02-19 15:56:57 +0100

    feat: add foreground propagation for uninstall cmd
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- pkg/uninstall/uninstall.go
+++ pkg/uninstall/uninstall.go
@@ -5,10 +5,13 @@ import (
 
 	"github.com/glasskube/glasskube/api/v1alpha1"
 	"github.com/glasskube/glasskube/pkg/client"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+var deletePropagationForeground = metav1.DeletePropagationForeground
+
 func Uninstall(pkgClient *client.PackageV1Alpha1Client, ctx context.Context, pkg *v1alpha1.Package) error {
-	err := pkgClient.Packages().Delete(ctx, pkg)
+	err := pkgClient.Packages().Delete(ctx, pkg, metav1.DeleteOptions{PropagationPolicy: &deletePropagationForeground})
 	if err != nil {
 		return err
 	}
