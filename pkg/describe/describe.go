13f9e602793b9dbf54b143be535bb5b9a853b991
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-02-20 14:57:50 +0100

    fix(ui): use real PackageInfo name when getting installed manifest
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- pkg/describe/describe.go
+++ pkg/describe/describe.go
@@ -6,7 +6,6 @@ import (
 	"github.com/glasskube/glasskube/api/v1alpha1"
 	"github.com/glasskube/glasskube/internal/repo"
 	"github.com/glasskube/glasskube/pkg/client"
-	"github.com/glasskube/glasskube/pkg/list"
 	"github.com/glasskube/glasskube/pkg/manifest"
 	"k8s.io/apimachinery/pkg/api/errors"
 )
@@ -16,26 +15,26 @@ func DescribePackage(
 	pkgName string,
 ) (*v1alpha1.Package, *client.PackageStatus, *v1alpha1.PackageManifest, error) {
 	pkgClient := client.FromContext(ctx)
-	pkg, err := list.Get(pkgClient, ctx, pkgName)
-	if err != nil && !errors.IsNotFound(err) {
-		return nil, nil, nil, err
-	}
+	var pkg v1alpha1.Package
 	var status *client.PackageStatus
 	var returnedManifest v1alpha1.PackageManifest
-	if pkg != nil {
+	if err := pkgClient.Packages().Get(ctx, pkgName, &pkg); err != nil && !errors.IsNotFound(err) {
+		return nil, nil, nil, err
+	} else if err == nil {
 		// pkg installed: use installed manifest
 		status = client.GetStatusOrPending(&pkg.Status)
-		installedManifest, err := manifest.GetInstalledManifest(ctx, pkgName)
+		installedManifest, err := manifest.GetInstalledManifestForPackage(ctx, pkg)
 		if err != nil {
 			return nil, nil, nil, err
 		}
 		returnedManifest = *installedManifest
 	} else {
+
 		// pkg not installed: use manifest from repo
 		_, err = repo.FetchLatestPackageManifest("", pkgName, &returnedManifest)
 		if err != nil {
 			return nil, nil, nil, err
 		}
 	}
-	return pkg, status, &returnedManifest, nil
+	return &pkg, status, &returnedManifest, nil
 }
