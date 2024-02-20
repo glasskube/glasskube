edccf3de0c3b910fadc915535e127bdfab26c21a
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-02-20 14:57:50 +0100

    fix(ui): use real PackageInfo name when getting installed manifest
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- pkg/manifest/manifest.go
+++ pkg/manifest/manifest.go
@@ -2,6 +2,7 @@ package manifest
 
 import (
 	"context"
+	"errors"
 
 	"github.com/glasskube/glasskube/pkg/client"
 
@@ -10,9 +11,21 @@ import (
 
 func GetInstalledManifest(ctx context.Context, pkgName string) (*v1alpha1.PackageManifest, error) {
 	pkgClient := client.FromContext(ctx)
+	var pkg v1alpha1.Package
+	if err := pkgClient.Packages().Get(ctx, pkgName, &pkg); err != nil {
+		return nil, err
+	}
+	return GetInstalledManifestForPackage(ctx, pkg)
+}
+
+func GetInstalledManifestForPackage(ctx context.Context, pkg v1alpha1.Package) (*v1alpha1.PackageManifest, error) {
+	pkgClient := client.FromContext(ctx)
+	if len(pkg.Status.OwnedPackageInfos) == 0 {
+		return nil, errors.New("Package has no owned PackageInfo")
+	}
+	packageInfoName := pkg.Status.OwnedPackageInfos[len(pkg.Status.OwnedPackageInfos)-1].Name
 	var packageInfo v1alpha1.PackageInfo
-	// TODO: Change this to use the actual package info name instead of the package name
-	if err := pkgClient.PackageInfos().Get(ctx, pkgName, &packageInfo); err != nil {
+	if err := pkgClient.PackageInfos().Get(ctx, packageInfoName, &packageInfo); err != nil {
 		return nil, err
 	} else {
 		return packageInfo.Status.Manifest, nil
