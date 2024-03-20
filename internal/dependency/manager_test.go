faf2766417a0df42119cda27da32f476592387c9
christoph <christoph.enne@glasskube.eu>
2024-03-20 14:49:38 +0100

    test: add tests for version range checking

--- internal/dependency/manager_test.go
+++ internal/dependency/manager_test.go
@@ -497,4 +497,82 @@ var _ = Describe("Dependency Manager", func() {
 
 	})
 
+	// TODO test IsUpdateAllowed()
+
+	Describe("CheckConflict", func() {
+
+		DescribeTable("Checking version ranges",
+			func(versionRange string, version string, valid bool) {
+				conflict, err := dm.CheckConflict(version, v1alpha1.Dependency{Version: versionRange})
+				if valid {
+					Expect(conflict).To(BeNil())
+					Expect(err).NotTo(HaveOccurred())
+				} else {
+					Expect(conflict).NotTo(BeNil())
+					Expect(err).NotTo(HaveOccurred())
+				}
+			},
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.2.0", true),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.2.0+0", true),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.2.4", true),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.2.9", true),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.1.9", false),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.2.0-rc.1", false),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.3.0-alpha.1", false),
+			Entry("When minor version is pinned", ">=1.2.0 <1.3.0", "1.3.0", false),
+
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.2.0", true),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.2.0+0", true),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.2.4", true),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.2.9", true),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.1.9", false),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.2.0-rc.1", false),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.3.0-alpha.1", false),
+			Entry("When minor version is pinned with ~", "~1.2.0", "1.3.0", false),
+
+			Entry("When minor version is pinned with ~", "~1.2", "1.2.0", true),
+			Entry("When minor version is pinned with ~", "~1.2", "1.2.0+0", true),
+			Entry("When minor version is pinned with ~", "~1.2", "1.2.4", true),
+			Entry("When minor version is pinned with ~", "~1.2", "1.2.9", true),
+			Entry("When minor version is pinned with ~", "~1.2", "1.1.9", false),
+			Entry("When minor version is pinned with ~", "~1.2", "1.2.0-rc.1", false),
+			Entry("When minor version is pinned with ~", "~1.2", "1.3.0-alpha.1", false),
+			Entry("When minor version is pinned with ~", "~1.2", "1.3.0", false),
+
+			Entry("When minor version is pinned with .x", "1.2.x", "1.2.0", true),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.2.0+0", true),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.2.4", true),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.2.9", true),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.1.9", false),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.2.0-rc.1", false),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.3.0-alpha.1", false),
+			Entry("When minor version is pinned with .x", "1.2.x", "1.3.0", false),
+
+			Entry("When major version is pinned with ^", "^1", "1.0.0", true),
+			Entry("When major version is pinned with ^", "^1", "1.3.5", true),
+			Entry("When major version is pinned with ^", "^1", "1.7.9+1", true),
+			Entry("When major version is pinned with ^", "^1", "1.0.0-rc.1", false),
+			Entry("When major version is pinned with ^", "^1", "2.0.0-rc.1", false),
+			Entry("When major version is pinned with ^", "^1", "2.0.0", false),
+
+			Entry("When major version is pinned with .x", "1.x", "1.0.0", true),
+			Entry("When major version is pinned with .x", "1.x", "1.3.5", true),
+			Entry("When major version is pinned with .x", "1.x", "1.7.9+1", true),
+			Entry("When major version is pinned with .x", "1.x", "1.0.0-rc.1", false),
+			Entry("When major version is pinned with .x", "1.x", "2.0.0-rc.1", false),
+			Entry("When major version is pinned with .x", "1.x", "2.0.0", false),
+
+			Entry("When a prerelease is the minimum", ">= 1.0.0-alpha.1", "1.0.0-alpha.1", true),
+			Entry("When a prerelease is the minimum", ">= 1.0.0-alpha.1", "1.0.0-beta.0", true),
+			Entry("When a prerelease is the minimum", ">= 1.0.0-alpha.1", "1.0.0-rc.0", true),
+			Entry("When a prerelease is the minimum", ">= 1.0.0-alpha.1", "1.0.0+0", true),
+			Entry("When a prerelease is the minimum", ">= 1.0.0-alpha.1", "3.0.0", true),
+			Entry("When a prerelease is the minimum", ">= 1.0.0-alpha.1", "1.0.0-alpha.0", false),
+
+			Entry("When a minimum contains a build number", ">= 1.0.0+3", "1.0.0+3", true),
+			Entry("When a minimum contains a build number", ">= 1.0.0+3", "1.0.0+2", true), // TODO to be fixed with #405
+		)
+
+	})
+
 })
