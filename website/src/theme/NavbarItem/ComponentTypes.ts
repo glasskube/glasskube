2e0e3a56eb6f16637b05210fba69f056e3ff6e45
Philip Miglinci <pmig@glasskube.eu>
2024-04-22 19:25:01 +0200

    docs(website): include GitHub star button in header (#459)
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

---
+++ website/src/theme/NavbarItem/ComponentTypes.ts
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import NavbarWrapper from '@site/src/theme/NavbarItem/NavbarWrapper';
+
+export default {
+  ...ComponentTypes,
+  'custom-wrapper': NavbarWrapper,
+};
