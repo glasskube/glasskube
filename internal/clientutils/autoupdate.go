02ccfb7cd3a706090974a81ac0d2d57e2e7cffab
UtkarshUmre <112888849+UtkarshUmre@users.noreply.github.com>
2024-05-25 17:48:56 +0000

    refactor: auto-update label to annotation
    Signed-off-by: UtkarshUmre <112888849+UtkarshUmre@users.noreply.github.com>

--- internal/clientutils/autoupdate.go
+++ internal/clientutils/autoupdate.go
@@ -8,8 +8,8 @@ import (
 
 func AutoUpdateString(pkg *v1alpha1.Package, disabledStr string) string {
 	if pkg != nil {
-		if pkg.Labels != nil {
-			autoUpdateValue, ok := pkg.Labels["packages.glasskube.dev/auto-update"]
+		if pkg.Annotations != nil {
+			autoUpdateValue, ok := pkg.Annotations["packages.glasskube.dev/auto-update"]
 			autoUpdateBool, _ := strconv.ParseBool(autoUpdateValue)
 			if ok && autoUpdateBool {
 				return "Enabled"
