089dda78f1b77da8ebb31181ebed0a5783498350
christoph <christoph.enne@glasskube.eu>
2024-06-07 10:52:17 +0200

    fix(website): avoid full page crash
    Signed-off-by: christoph <christoph.enne@glasskube.eu>

--- website/src/theme/cloud-banner.js
+++ website/src/theme/cloud-banner.js
@@ -1,15 +1,17 @@
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
-export function onRouteDidUpdate({ location, previousLocation }) {
+export function onRouteDidUpdate({location, previousLocation}) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
   if (location.pathname !== previousLocation?.pathname) {
     if (ExecutionEnvironment.canUseDOM) {
-      const id = window.posthog?.get_distinct_id();
-      const elem = document.querySelector('#banner-cloud-link');
-      if (elem) {
-        elem.href = elem.href + id;
-      }
+      try {
+        const id = window.posthog.get_distinct_id();
+        const elem = document.querySelector('#banner-cloud-link');
+        if (elem) {
+          elem.href = elem.href + id;
+        }
+      } catch (e) {}
     }
   }
 }
