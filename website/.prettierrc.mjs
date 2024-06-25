a8afd40d86f4ac17ae9df49458ad53a5384eb91b
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-06-25 18:43:45 +0530

    style(website): configure eslint with docusaurus, react-ts and prettier plugins (#80)
    Signed-off-by: Mridul Dhiman <dhimanmridul91@gmail.com>

---
+++ website/.prettierrc.mjs
@@ -0,0 +1,11 @@
+export default {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 80,
+};
---
+++ website/eslint.config.mjs
@@ -0,0 +1,50 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import * as tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import prettierEslintPlugin from "eslint-plugin-prettier";
+import docusaurusEslintPlugin from "@docusaurus/eslint-plugin";
+
+export default [
+  // TODO: eslint configure for .md, .mdx files
+  {files: ["*/.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {ignores: ["babel.config.js", "eslint.config.mjs", "docusaurus.config.ts"]},
+  {
+    plugins: {
+      prettier: prettierEslintPlugin,
+      "@docusaurus": docusaurusEslintPlugin,
+    },
+  },
+  {
+    languageOptions: {
+      parser: tseslint.parser, 
+      parserOptions: {
+        ecmaFeatures: {jsx: true},
+        sourceType: "module"
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    rules: {
+      "prettier/prettier": "off",
+      "@docusaurus/string-literal-i18n-messages": "warn",
+      "@docusaurus/no-untranslated-text": "warn",
+      "@docusaurus/no-html-links": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
