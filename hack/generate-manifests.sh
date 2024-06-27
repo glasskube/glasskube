9f338c002454554b70fdeb6cc9fa4ffab2c63f25
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-06-27 16:32:49 +0200

    feat(deploy): add autoupdate manifest
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- hack/generate-manifests.sh
+++ hack/generate-manifests.sh
@@ -3,7 +3,7 @@ CONFIG_DIR="config"
 OUT_DIR="build/manifest"
 rm -rf "$OUT_DIR"
 mkdir -p "$OUT_DIR"
-for VARIANT in "aio" "slim"; do
+for VARIANT in "aio" "slim" "autoupdater"; do
   OUT_FILE="$OUT_DIR/manifest-$VARIANT.yaml ..."
   echo "Generating $OUT_FILE"
   kubectl kustomize "$CONFIG_DIR/$VARIANT" > "$OUT_FILE"
