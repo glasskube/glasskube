420f9c3d8f86c42cc3b9f611fe5912cbe22e336d
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-07-05 11:55:28 +0200

    fix(cli): do not require bootstrap for `glasskube auto-update`

--- cmd/glasskube/cmd/auto-update.go
+++ cmd/glasskube/cmd/auto-update.go
@@ -35,7 +35,7 @@ var autoUpdateEnableCmd = &cobra.Command{
 var autoUpdateDisableCmd = &cobra.Command{
 	Use:               "disable [...package]",
 	Short:             "Disable automatic updates for packages:",
-	PreRun:            cliutils.SetupClientContext(false, &rootCmdOptions.SkipUpdateCheck),
+	PreRun:            cliutils.SetupClientContext(true, &rootCmdOptions.SkipUpdateCheck),
 	ValidArgsFunction: completeInstalledPackageNames,
 	Run: runAutoUpdateEnableOrDisable(false,
 		"Enable automatic updates for the following packages", "Automatic updates disabled"),
@@ -140,7 +140,7 @@ var autoUpdateCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	PreRun: cliutils.RunAll(
 		func(c *cobra.Command, s []string) { config.NonInteractive = true },
-		cliutils.SetupClientContext(true, &rootCmdOptions.SkipUpdateCheck),
+		cliutils.SetupClientContext(false, &rootCmdOptions.SkipUpdateCheck),
 	),
 	Run: runAutoUpdate,
 }
