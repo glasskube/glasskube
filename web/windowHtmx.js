7cdf49e03fa393a95778b2b324c267aca3a4fc5b
Christoph Enne <christoph.enne@glasskube.eu>
2024-07-08 13:47:33 +0200

    chore(ui): update htmx to v2 (#962)
    Signed-off-by: christoph <christoph.enne@glasskube.eu>

--- web/windowHtmx.js
+++ web/windowHtmx.js
@@ -1,4 +1,4 @@
 // This file is required to provide the global htmx variable.
 // Reference: https://htmx.org/docs/#webpack
-import htmx from 'htmx.org';
+import htmx from 'htmx.org/dist/htmx.esm';
 window.htmx = htmx;
