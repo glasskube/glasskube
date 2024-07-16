67afb30926b79f0131d5e9ff5b3d66f0d32109fa
jakepage91 <jake@glasskube.com>
2024-07-16 10:52:48 +0100

    docs(website): added kube-prometheus-stack guide + corrections

--- website/sidebar-guides.ts
+++ website/sidebar-guides.ts
@@ -12,7 +12,7 @@ const sidebars: SidebarsConfig = {
           '⚠️ Learn how to install your favorite Kubernetes add-ons using the Glasskube package manager ⚠️',
         slug: '/categories/',
       },
-      items: ['cert-manager', 'ingress-nginx', 'rabbitmq', 'quickwit'],
+      items: ['cert-manager', 'ingress-nginx', 'rabbitmq', 'quickwit', 'kube-prom-stack'],
     },
     {
       type: 'category',
