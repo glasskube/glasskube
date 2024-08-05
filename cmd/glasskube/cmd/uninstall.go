a01210d9d5db20b8bc7a5484b05cf644aaedc880
Hanshal Mehta <122217807+hanshal101@users.noreply.github.com>
2024-08-05 20:24:47 +0530

    feat(cli): add --dry-run to uninstall command
    Signed-off-by: hanshal101 <122217807+hanshal101@users.noreply.github.com>

--- cmd/glasskube/cmd/uninstall.go
+++ cmd/glasskube/cmd/uninstall.go
@@ -17,6 +17,7 @@ var uninstallCmdOptions = struct {
 	Yes    bool
 	KindOptions
 	NamespaceOptions
+	DryRunOptions
 }{
 	KindOptions: DefaultKindOptions(),
 }
@@ -67,13 +68,13 @@ var uninstallCmd = &cobra.Command{
 		}
 
 		if uninstallCmdOptions.NoWait {
-			if err := uninstaller.Uninstall(ctx, pkg); err != nil {
+			if err := uninstaller.Uninstall(ctx, pkg, uninstallCmdOptions.DryRun); err != nil {
 				fmt.Fprintf(os.Stderr, "\n❌ An error occurred during uninstallation:\n\n%v\n", err)
 				cliutils.ExitWithError()
 			}
 			fmt.Fprintln(os.Stderr, "Uninstallation started in background")
 		} else {
-			if err := uninstaller.UninstallBlocking(ctx, pkg); err != nil {
+			if err := uninstaller.UninstallBlocking(ctx, pkg, uninstallCmdOptions.DryRun); err != nil {
 				fmt.Fprintf(os.Stderr, "\n❌ An error occurred during uninstallation:\n\n%v\n", err)
 				cliutils.ExitWithError()
 			}
