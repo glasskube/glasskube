7b2b8b9573a23a555752caaba3ea4aecd3b1cfce
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-08-21 10:43:20 -0700

    chore(website): fix build

--- website/src/theme/BlogPostItem/index.tsx
+++ website/src/theme/BlogPostItem/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type {WrapperProps} from '@docusaurus/types';
-import {useBlogPost} from '@docusaurus/theme-common/internal';
+import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
 import {BlogDiscussion} from '@site/src/components/GiscusWrapper';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
