70033d57a24612f2f70482ae793c3bee3e5eec3f
Prikshit Singh <99068054+prikshitsingh24@users.noreply.github.com>
2024-09-08 23:15:29 +0530

    docs(website): improve Blog page Layout (#1153)
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>
    Co-authored-by: Philip Miglinci <pmig@glasskube.eu>

---
+++ website/src/theme/BlogSidebar/index.tsx
@@ -0,0 +1,18 @@
+import Link from '@docusaurus/Link';
+import React from 'react';
+
+export default function BlogSidebar(): JSX.Element {
+  return (
+    <div>
+      <div style={{fontSize: '28px'}}>Recent posts</div>
+      <div>
+        <Link
+          style={{marginTop: '20px'}}
+          className={`button button--outline`}
+          to="/blog">
+          View Recent posts
+        </Link>
+      </div>
+    </div>
+  );
+}
