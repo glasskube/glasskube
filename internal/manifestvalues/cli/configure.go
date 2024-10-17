a9230f33078410c1a73ab68fdbb1c630170e608a
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-10-17 11:05:01 +0200

    fix: plain-text AutoLink display and enably markdown linkify
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/manifestvalues/cli/configure.go
+++ internal/manifestvalues/cli/configure.go
@@ -14,6 +14,7 @@ import (
 	"github.com/glasskube/glasskube/internal/maputils"
 	"github.com/glasskube/glasskube/internal/util"
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/renderer"
 	goldmarkutil "github.com/yuin/goldmark/util"
 )
@@ -168,6 +169,9 @@ func printHeader(name string, def v1alpha1.ValueDefinition) {
 
 func printMarkdown(w io.Writer, text string) {
 	md := goldmark.New(
+		goldmark.WithExtensions(
+			extension.Linkify,
+		),
 		goldmark.WithRenderer(renderer.NewRenderer(
 			renderer.WithNodeRenderers(
 				goldmarkutil.Prioritized(cliutils.MarkdownRenderer(), 1000),
