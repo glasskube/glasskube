a7478c799a7725c77c02414dd432c3092f86cf6e
Jake Page <38757612+jakepage91@users.noreply.github.com>
2024-11-12 00:43:51 -0800

    docs(website): add v1 Blog CTA component (#1374)
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>
    Co-authored-by: Philip Miglinci <pmig@glasskube.eu>

---
+++ website/src/components/BlogCTA/blogCTA.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import DemoButton from '../buttons/DemoButton';
+import TalkToFoundersButton from '../buttons/TalkToFoundersButton';
+import styles from './styles.module.css';
+import clsx from 'clsx';
+import SignupForWaitlistButton from '@site/src/components/buttons/SignupForWaitlistButton';
+
+export default function BlogCTA(): JSX.Element {
+  return (
+    <div className={clsx('margin-top--xl margin-bottom--lg', styles.ctaWrapper)}>
+      <div className="container">
+        <div className="row">
+          <div className="col col--10 col--offset-1">
+            <div className={styles.ctaContent}>
+              <h2>Need help running Kubeflow in production?</h2>
+              <p className="margin-bottom--lg">
+                Book a call with us to discuss your Kubernetes deployment and scaling needs for your Cloud-Native AI.
+              </p>
+              <div className={styles.buttonContainer}>
+                <SignupForWaitlistButton additionalClassNames="margin-right--md" />
+                <DemoButton additionalClassNames="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
