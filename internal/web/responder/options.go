fedc003aca5fa54286d67253c07845ca88609edf
christoph <christoph.enne@glasskube.eu>
2024-11-13 11:21:15 +0100

    refactor: remove unused statusCode
    Signed-off-by: christoph <christoph.enne@glasskube.eu>

--- internal/web/responder/options.go
+++ internal/web/responder/options.go
@@ -3,19 +3,12 @@ package responder
 import "github.com/glasskube/glasskube/internal/web/types"
 
 type response struct {
-	statusCode   int
 	templateData any
 	partialErr   error
 }
 
 // TODO merge with toast response option???
 type ResponseOption func(*response)
-
-func WithStatusCode(statusCode int) ResponseOption {
-	return func(r *response) {
-		r.statusCode = statusCode
-	}
-}
 
 func ContextualizedTemplate(templateData types.ContextInjectable) ResponseOption {
 	return func(r *response) {
