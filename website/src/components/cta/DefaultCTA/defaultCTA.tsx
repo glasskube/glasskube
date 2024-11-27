472aaec3a33af96c0fe3fc02ba4fa01524e4adf6
Philip Miglinci <pmig@glasskube.eu>
2024-11-27 13:35:45 +0100

    docs(website): reduce ctas on glossary
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

--- website/src/components/cta/DefaultCTA/defaultCTA.tsx
+++ website/src/components/cta/DefaultCTA/defaultCTA.tsx
@@ -3,7 +3,7 @@ import CustomCTA from '../CustomCTA/customCTA';
 export default function DefaultCTA(): JSX.Element {
   return (
     <CustomCTA
-      header="Are you deploying your software to your customers on-premises or your own edge locations?"
+      header="Are you deploying your software to customer-managed on-premises environments or your own edge locations?"
       text="Try Glasskube Cloud to get on top of your deployments, manage updates, handle communications of required infrastructure updates,
        perform pre-flight checks and automate your software delivery."
     />
