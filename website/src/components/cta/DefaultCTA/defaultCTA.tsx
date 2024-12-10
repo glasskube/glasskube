aab84bfc8b7b19eb2b167fa1e4ae68b03e0cc172
Philip Miglinci <pmig@glasskube.eu>
2024-12-10 13:58:57 +0100

    docs(website): fix default cta
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

--- website/src/components/cta/DefaultCTA/defaultCTA.tsx
+++ website/src/components/cta/DefaultCTA/defaultCTA.tsx
@@ -3,7 +3,7 @@ import CustomCTA from '../CustomCTA/customCTA';
 export default function DefaultCTA(): JSX.Element {
   return (
     <CustomCTA
-      header="Do you still think that offering on-premises installations are hard?"
+      header="Do you still think that offering on-premises installations is hard?"
       text="Glasskube Cloud is a battle tested software distribution platform that helps you scale from your first
        on-premises customers to dozens and even thousands."
     />
