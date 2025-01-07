1593534653a00e45e490eaa01a898402c11031e5
Philip Miglinci <pmig@glasskube.eu>
2025-01-07 12:17:39 +0100

    docs(website): move website to https://github.com/glasskube/glasskube.dev/ (#1479)
    Signed-off-by: Philip Miglinci <pmig@glasskube.eu>

--- website/src/theme/BlogPostItem/Container/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type {Props} from '@theme/BlogPostItem/Container';
-
-export default function BlogPostItemContainer({
-  children,
-  className,
-}: Props): JSX.Element {
-  return <article className={className}>{children}</article>;
-}
